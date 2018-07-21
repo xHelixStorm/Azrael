d9c6b7d6c2a407e93fdf70c89ad55dd9d67611b0
borrellg <borrellg@rhe4209.toennies.net>
2018-07-21 17:45:56 +0200

    class for the automated file deletion

---
+++ src/threads/DelayDelete.java
@@ -0,0 +1,25 @@
+package threads;
+
+import java.io.File;
+
+public class DelayDelete implements Runnable{
+	File file;
+	long delay;
+	
+	public DelayDelete(String _file, long _delay){
+		file = new File(_file);
+		delay = _delay;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if(file.exists()) {
+			file.delete();
+		}
+	}
+}
