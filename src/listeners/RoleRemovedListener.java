6bb4a02a7618fdbf04094cefa4dcff19c2421961
borrellg <borrellg@rhe4209.toennies.net>
2018-07-22 15:30:41 +0200

    AuditLog implemented
    The message now displays who has manually removed the mute role

--- src/listeners/RoleRemovedListener.java
+++ src/listeners/RoleRemovedListener.java
@@ -5,15 +5,31 @@ import java.sql.Timestamp;
 
 import core.UserPrivs;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.audit.AuditLogEntry;
+import net.dv8tion.jda.core.audit.AuditLogKey;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberRoleRemoveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.requests.restaction.pagination.AuditLogPaginationAction;
+import sql.ServerRoles;
 import sql.SqlConnect;
 
 public class RoleRemovedListener extends ListenerAdapter{
 	
 	@Override
 	public void onGuildMemberRoleRemove(GuildMemberRoleRemoveEvent e){
-		EmbedBuilder message = new EmbedBuilder().setColor(Color.ORANGE);
+		EmbedBuilder message = new EmbedBuilder().setColor(Color.ORANGE).setTitle("Mute role has been manually removed!");
+		
+		String trigger_user_name = "";
+		AuditLogPaginationAction logs = e.getGuild().getAuditLogs();
+		first_entry: for (AuditLogEntry entry : logs)
+		{
+			ServerRoles.SQLgetRole(e.getGuild().getIdLong(), "mut");
+			if(entry.getChangeByKey(AuditLogKey.MEMBER_ROLES_REMOVE).toString().contains(""+ServerRoles.getRole_ID()) && entry.getGuild().getIdLong() == e.getGuild().getIdLong() && entry.getTargetIdLong() == e.getMember().getUser().getIdLong()) {
+				trigger_user_name = entry.getUser().getName()+"#"+entry.getUser().getDiscriminator();
+			}
+			break first_entry;
+		}
+		
 		String member_name = e.getMember().getUser().getName()+"#"+e.getMember().getUser().getDiscriminator();
 		long user_id = e.getMember().getUser().getIdLong();
 		long guild_id = e.getGuild().getIdLong();
@@ -29,7 +45,7 @@ public class RoleRemovedListener extends ListenerAdapter{
 			log_channel_id = SqlConnect.getChannelID();
 			SqlConnect.clearUnmute();
 			
-			if(log_channel_id != 0){e.getGuild().getTextChannelById(log_channel_id).sendMessage(message.setDescription("["+timestamp.toString()+"] The mute role has been manually removed from **"+member_name+"** with the ID number **"+user_id+"**!").build()).queue();}
+			if(log_channel_id != 0){e.getGuild().getTextChannelById(log_channel_id).sendMessage(message.setDescription("["+timestamp.toString()+"] **"+trigger_user_name+"** has manually removed the mute role from **"+member_name+"** with the ID number **"+user_id+"**!").build()).queue();}
 		}
 		else if(!UserPrivs.isUserMuted(e.getUser(), guild_id) && SqlConnect.getUser_id() != 0){
 			SqlConnect.SQLUpdateMuted(user_id, guild_id, false);
