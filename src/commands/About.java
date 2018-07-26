039731de800a7aa4b67d3c4cc5565907abda4363
borrellg <borrellg@rhe4209.toennies.net>
2018-07-26 11:39:27 +0200

    About page updated in Embed with reference to GitHub

--- src/commands/About.java
+++ src/commands/About.java
@@ -1,13 +1,12 @@
 package commands;
 
 import fileManagement.IniFileReader;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import preparedMessages.AboutText;
 import sql.SqlConnect;
+import util.STATIC;
 
 public class About implements Command {
-	
-	private static String message = AboutText.getAbout();
 
 	@Override
 	public boolean called(String[] args, MessageReceivedEvent e) {
@@ -26,7 +25,19 @@ public class About implements Command {
 				e.getTextChannel().sendMessage(e.getMember().getAsMention()+" I'm not allowed to execute commands in this channel, please write it again in <#"+channel_id+">").queue();
 			}
 			else{
-				e.getTextChannel().sendMessage("```" + message + "```").queue();
+				EmbedBuilder messageBuilder = new EmbedBuilder().setColor(0x00AE86).setThumbnail(e.getJDA().getSelfUser().getEffectiveAvatarUrl()).setTitle("About Page!");
+				messageBuilder.setAuthor("Azrael", e.getJDA().getSelfUser().getEffectiveAvatarUrl());
+				messageBuilder.setDescription("Here are all details about myself!");
+				messageBuilder.addField("BOT VERSION", STATIC.getVersion_New(), true);
+				messageBuilder.addField("DEVELOPER", "Java developer xHelixStorm", true);
+				messageBuilder.addBlankField(false);
+				messageBuilder.addField("Functionalities", "- Self designed ranking system.\n"
+						+ "- Various bot settings for numerous moderation tools such as an automated mute system, bad-word filter, name filter and much more.\n"
+						+ "- Entertainment commands to display your current level, ranking gain dailies, purchase from an integrated shop, take a look at your inventory or simply bring up cute pictures of pugs or cats.\n\n"
+						+ "You can see all enabled commands under "+IniFileReader.getCommandPrefix()+"commands", false);
+				messageBuilder.addBlankField(false);
+				messageBuilder.addField("SOURCE CODE", "[Check the latest updates of Azrael on GitHub!](https://github.com/xHelixStorm/Azrael)", false);
+				e.getTextChannel().sendMessage(messageBuilder.build()).queue();
 			}
 		}
 	}
