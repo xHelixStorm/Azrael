ac18264f806a9c560b140f108460d17d03d7a3f4
Giuseppe-Emanuele <Giuseppe-Emanuele@DESKTOP-K48T6BM.fritz.box>
2018-08-22 00:11:07 +0200

    progress for the notification of removed messages (not done)

--- src/core/Hashes.java
+++ src/core/Hashes.java
@@ -17,6 +17,7 @@ public class Hashes {
     };
     
     private static final Map<String, ArrayList<String>> querry_result = new HashMap<String, ArrayList<String>>();
+    private static final Map<Long, Long> message_removed = new HashMap<Long, Long>();
 	
 	public static void addMessagePool(long _message_id, String _message) {
 		message_pool.put(_message_id, _message);
@@ -24,11 +25,17 @@ public class Hashes {
 	public static void addQuerryResult(String _key, ArrayList<String> _result) {
 		querry_result.put(_key, _result);
 	}
+	public static void addMessageRemoved(Long _key, Long _message_id){
+		message_removed.put(_key, _message_id);
+	}
 	public static String getMessagePool(long _message_id) {
 		return message_pool.get(_message_id);
 	}
 	public static ArrayList<String> getQuerryResult(String _key) {
 		return querry_result.get(_key);
+	}
+	public static Long getMessageRemoved(Long _key){
+		return message_removed.get(_key);
 	}
 	public static void removeMessagePool(long _message_id) {
 		message_pool.remove(_message_id);
--- src/listeners/MessageRemovedListener.java
+++ src/listeners/MessageRemovedListener.java
@@ -1,7 +1,6 @@
 package listeners;
 
 import java.awt.Color;
-import java.sql.Timestamp;
 
 import core.Hashes;
 import core.UserPrivs;
@@ -21,16 +20,16 @@ public class MessageRemovedListener extends ListenerAdapter{
 		String trigger_user_id = "";
 		String trigger_user_name = "";
 		String removed_from = "";
+		long audit_id = 0;
 		AuditLogPaginationAction logs = e.getGuild().getAuditLogs();
 		first_entry: for (AuditLogEntry entry : logs)
 		{
-			long audit = Timestamp.valueOf(entry.getCreationTime().toLocalDateTime()).getTime();
-			long now = new Timestamp(System.currentTimeMillis()).getTime();
-			
-			if(entry.getType().toString().equals("MESSAGE_DELETE") && entry.getGuild().getIdLong() == e.getGuild().getIdLong() && (now - audit) < 2000) {
+			if(entry.getType().toString().equals("MESSAGE_DELETE") && entry.getGuild().getIdLong() == e.getGuild().getIdLong() && (Hashes.getMessageRemoved(entry.getIdLong()) == null || Hashes.getMessageRemoved(entry.getIdLong()) == 0)) {
 				trigger_user_id = entry.getUser().getId();
 				trigger_user_name = entry.getUser().getName()+"#"+entry.getUser().getDiscriminator();
 				removed_from = entry.getTargetId();
+				audit_id = entry.getIdLong();
+				Hashes.addMessageRemoved(audit_id, (long) 0);
 			}
 			break first_entry;
 		}
@@ -38,14 +37,17 @@ public class MessageRemovedListener extends ListenerAdapter{
 		long message_id = e.getMessageIdLong();
 		String removed_message = Hashes.getMessagePool(message_id);
 		
-		if(removed_message != null && removed_message.length() > 0) {
-			Hashes.removeMessagePool(message_id);
-			if(trigger_user_id.length() > 0) {
-				if((UserPrivs.isUserAdmin(e.getGuild().getMemberById(trigger_user_id).getUser(), e.getGuild().getIdLong()) || UserPrivs.isUserMod(e.getGuild().getMemberById(trigger_user_id).getUser(), e.getGuild().getIdLong())) && !UserPrivs.isUserBot(e.getGuild().getMemberById(removed_from).getUser(), e.getGuild().getIdLong()) && !trigger_user_id.equals(removed_from)) {
-					message.setTitle(trigger_user_name+" has removed a message from #"+e.getTextChannel().getName()+"!");
-					SqlConnect.SQLgetChannelID(e.getGuild().getIdLong(), "tra");
-					if(removed_message.length() > 0) {
-						if(SqlConnect.getChannelID() != 0){e.getGuild().getTextChannelById(SqlConnect.getChannelID()).sendMessage(message.setDescription(removed_message).build()).queue();}
+		if(Hashes.getMessageRemoved(audit_id) == 0){
+			Hashes.addMessageRemoved(audit_id, e.getMessageIdLong());
+			if(removed_message != null && removed_message.length() > 0) {
+				Hashes.removeMessagePool(message_id);
+				if(trigger_user_id.length() > 0) {
+					if((UserPrivs.isUserAdmin(e.getGuild().getMemberById(trigger_user_id).getUser(), e.getGuild().getIdLong()) || UserPrivs.isUserMod(e.getGuild().getMemberById(trigger_user_id).getUser(), e.getGuild().getIdLong())) && !UserPrivs.isUserBot(e.getGuild().getMemberById(removed_from).getUser(), e.getGuild().getIdLong()) && !trigger_user_id.equals(removed_from)) {
+						message.setTitle(trigger_user_name+" has removed a message from #"+e.getTextChannel().getName()+"!");
+						SqlConnect.SQLgetChannelID(e.getGuild().getIdLong(), "tra");
+						if(removed_message.length() > 0) {
+							if(SqlConnect.getChannelID() != 0){e.getGuild().getTextChannelById(SqlConnect.getChannelID()).sendMessage(message.setDescription(removed_message).build()).queue();}
+						}
 					}
 				}
 			}
