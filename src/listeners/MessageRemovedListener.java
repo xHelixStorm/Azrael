2d2788874375d7e3f31da84b48130f9650f9a0f0
Giuseppe-Emanuele <Giuseppe-Emanuele@DESKTOP-K48T6BM.fritz.box>
2018-08-25 14:09:21 +0200

    functionality improvement

--- src/listeners/MessageRemovedListener.java
+++ src/listeners/MessageRemovedListener.java
@@ -17,39 +17,59 @@ public class MessageRemovedListener extends ListenerAdapter{
 	public void onMessageDelete(MessageDeleteEvent e) {
 		EmbedBuilder message = new EmbedBuilder().setColor(Color.ORANGE);
 		
+		int audit_counter = 0;
 		String trigger_user_id = "";
 		String trigger_user_name = "";
 		String removed_from = "";
 		boolean send_message = false;
 		long audit_id = 0;
 		AuditLogPaginationAction logs = e.getGuild().getAuditLogs();
-		first_entry: for (AuditLogEntry entry : logs)
+		end_entry: for (AuditLogEntry entry : logs)
 		{
-			if(Hashes.getMessageRemoved(entry.getIdLong()) == null || Hashes.getMessageRemoved(entry.getIdLong()) < Integer.parseInt(entry.getOptionByName("count"))){
-				send_message = true;
+			if(audit_counter != 3){
+				if(entry.getType().toString().equals("MESSAGE_DELETE")){
+					if(Hashes.getMessageRemoved(entry.getIdLong()) == null){
+						Hashes.addMessageRemoved(entry.getIdLong(), 0);
+					}
+					if(Hashes.getMessageRemoved(entry.getIdLong()) == (Integer.parseInt(entry.getOptionByName("count")) - 1)){
+						send_message = true;
+					}
+					if(send_message == true && e.getTextChannel().getIdLong() == Long.parseLong(entry.getOptionByName("channel_id").toString()) && (UserPrivs.isUserAdmin(e.getGuild().getMemberById(entry.getUser().getId()).getUser(), e.getGuild().getIdLong()) || UserPrivs.isUserMod(e.getGuild().getMemberById(entry.getUser().getId()).getUser(), e.getGuild().getIdLong()))) {
+						removed_from = entry.getTargetId();
+						if(Hashes.getMessagePool(e.getMessageIdLong()).contains(e.getGuild().getMemberById(removed_from).getEffectiveName())){
+							if(!UserPrivs.isUserBot(e.getGuild().getMemberById(removed_from).getUser(), e.getGuild().getIdLong())){
+								trigger_user_id = entry.getUser().getId();
+								trigger_user_name = entry.getUser().getName()+"#"+entry.getUser().getDiscriminator();
+								audit_id = entry.getIdLong();
+								Hashes.addMessageRemoved(audit_id, Integer.parseInt(entry.getOptionByName("count")));
+								break end_entry;
+							}
+							else{
+								Hashes.removeMessageRemoved(entry.getIdLong());
+							}
+						}
+					}
+				}
+				audit_counter++;
 			}
-			
-			if(entry.getType().toString().equals("MESSAGE_DELETE") && send_message == true && entry.getGuild().getIdLong() == e.getGuild().getIdLong() && e.getTextChannel().getIdLong() == Long.parseLong(entry.getOptionByName("channel_id").toString()) && (UserPrivs.isUserAdmin(e.getGuild().getMemberById(entry.getUser().getId()).getUser(), e.getGuild().getIdLong()) || UserPrivs.isUserMod(e.getGuild().getMemberById(entry.getUser().getId()).getUser(), e.getGuild().getIdLong()))) {
-				trigger_user_id = entry.getUser().getId();
-				trigger_user_name = entry.getUser().getName()+"#"+entry.getUser().getDiscriminator();
-				removed_from = entry.getTargetId();
-				audit_id = entry.getIdLong();
-				Hashes.addMessageRemoved(audit_id, Integer.parseInt(entry.getOptionByName("count")));
-				break first_entry;
+			else {
+				break end_entry;
 			}
 		}
 		
 		long message_id = e.getMessageIdLong();
 		String removed_message = Hashes.getMessagePool(message_id);
 		
-		if(send_message == true && !UserPrivs.isUserBot(e.getGuild().getMemberById(removed_from).getUser(), e.getGuild().getIdLong()) && !trigger_user_id.equals(removed_from)){
-			if(removed_message != null && removed_message.length() > 0) {
-				Hashes.removeMessagePool(message_id);
-				if(trigger_user_id.length() > 0) {
-					message.setTitle(trigger_user_name+" has removed a message from #"+e.getTextChannel().getName()+"!");
-					SqlConnect.SQLgetChannelID(e.getGuild().getIdLong(), "tra");
-					if(removed_message.length() > 0) {
-						if(SqlConnect.getChannelID() != 0){e.getGuild().getTextChannelById(SqlConnect.getChannelID()).sendMessage(message.setDescription(removed_message).build()).queue();}
+		if(send_message == true && removed_from != null){
+			if(!trigger_user_id.equals(removed_from)){
+				if(removed_message != null && removed_message.length() > 0) {
+					Hashes.removeMessagePool(message_id);
+					if(trigger_user_id.length() > 0) {
+						message.setTitle(trigger_user_name+" has removed a message from #"+e.getTextChannel().getName()+"!");
+						SqlConnect.SQLgetChannelID(e.getGuild().getIdLong(), "tra");
+						if(removed_message.length() > 0) {
+							if(SqlConnect.getChannelID() != 0){e.getGuild().getTextChannelById(SqlConnect.getChannelID()).sendMessage(message.setDescription(removed_message).build()).queue();}
+						}
 					}
 				}
 			}
