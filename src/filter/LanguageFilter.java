f01f045dcc5a30b9b82efb205fa455a44cd878b9
Giuseppe-Emanuele <Giuseppe-Emanuele@DESKTOP-K48T6BM.fritz.box>
2018-09-25 00:11:45 +0200

    language update and test db for the ranking system

--- src/filter/LanguageFilter.java
+++ src/filter/LanguageFilter.java
@@ -32,6 +32,27 @@ public class LanguageFilter extends ListenerAdapter implements Runnable{
 		EmbedBuilder message = new EmbedBuilder().setColor(Color.ORANGE).setTitle("Message removed!");
 		boolean wordFound = false;
 		boolean exceptionFound = false;
+		String [] output = new String[2];
+		
+		if(filter_lang.size() == 1){
+			switch(filter_lang.get(0)){
+				case "ger": 
+					output[0] = " Die Nachricht wurde wegen schlechtes benehmen entfernt!";
+					output[1] = " Dies ist deine zweite Warnung. Eine weitere entfernte Nachricht und du wirst auf diesem Server **stumm geschaltet**!";
+					break;
+				case "fre":
+					output[0] = " Votre message à été supprimé pour mauvais comportement !";
+					output[1] = " C'est votre deuxième avertissement. Encore une fois et vous serez **mis sous silence** sur le serveur !";
+					break;
+				default:
+					output[0] = " Message has been removed due to bad behaviour!";
+					output[1] = " This has been the second warning. One more and you'll be **muted** from the server!";
+			}
+		}
+		else{
+			output[0] = " Message has been removed due to bad behaviour!";
+			output[1] = " This has been the second warning. One more and you'll be **muted** from the server!";
+		}
 		
 		if(!UserPrivs.isUserBot(e.getMember().getUser(), e.getGuild().getIdLong())){
 			String getMessage = e.getMessage().getContentRaw();
@@ -76,14 +97,14 @@ public class LanguageFilter extends ListenerAdapter implements Runnable{
 				Path path = Paths.get(IniFileReader.getTempDirectory()+"Reports/"+filename.toString()+".azr");
 				
 				if(Files.notExists(path)){
-					e.getTextChannel().sendMessage(e.getMember().getAsMention()+" Message has been removed due to bad behaviour!").queue();
+					e.getTextChannel().sendMessage(e.getMember().getAsMention()+" "+output[0]).queue();
 					FileSetting.createFile(IniFileReader.getTempDirectory()+"Reports/"+filename.toString()+".azr", "1");
 					new Thread(new DelayDelete(IniFileReader.getTempDirectory()+"Reports/"+filename.toString()+".azr", 300000)).start();
 				}
 				else if (Files.exists(path)){
 					report = FileSetting.readFile(IniFileReader.getTempDirectory()+"Reports/"+filename.toString()+".azr");
 					if(report.contains("1")){
-						e.getTextChannel().sendMessage(e.getMember().getAsMention()+" This has been the second warning. One more and you'll be **muted** from the server!").queue();
+						e.getTextChannel().sendMessage(e.getMember().getAsMention()+" "+output[1]).queue();
 						FileSetting.createFile(IniFileReader.getTempDirectory()+"Reports/"+filename.toString()+".azr", "2");
 					}
 					else if (report.contains("2")){
