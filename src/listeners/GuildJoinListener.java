96c4263ad5d7709dd264864a6b2ccf245101bed4
Giuseppe-Emanuele <Giuseppe-Emanuele@DESKTOP-K48T6BM.fritz.box>
2019-05-04 12:22:43 +0200

    see previous commit

--- src/listeners/GuildJoinListener.java
+++ src/listeners/GuildJoinListener.java
@@ -1,11 +1,15 @@
 package listeners;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import sql.RankingSystem;
+import threads.BotStartAssign;
 import sql.DiscordRoles;
 import sql.Patchnotes;
 import sql.Azrael;
@@ -34,6 +38,9 @@ public class GuildJoinListener extends ListenerAdapter{
 		if(Patchnotes.SQLInsertGuild(guild_id, guild_name) == 0) {
 			logger.error("guild information couldn't be inserted into DiscordRoles.guilds table for the guild {}", guild_name);
 		}
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(new BotStartAssign(null, e));
+		executor.shutdown();
 		Azrael.SQLInsertActionLog("GUILD_JOIN", e.getGuild().getIdLong(), e.getGuild().getIdLong(), guild_name);
 	}
 }
