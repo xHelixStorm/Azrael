86561a2fa2ee8919caf4efe4ccbe5b331c4e88d1
Giuseppe-Emanuele <Giuseppe-Emanuele@DESKTOP-K48T6BM.fritz.box>
2019-08-30 00:00:11 +0200

    functionality improved for the boost counter

--- src/listeners/BoostCountListener.java
+++ src/listeners/BoostCountListener.java
@@ -1,9 +1,7 @@
 package listeners;
 
 import java.awt.Color;
-import java.sql.Timestamp;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
@@ -16,30 +14,54 @@ public class BoostCountListener extends ListenerAdapter{
 	
 	@Override
 	public void onGuildUpdateBoostCount(GuildUpdateBoostCountEvent e) {
-		List<Member> members = e.getGuild().getMembers().parallelStream().filter(f -> f.getTimeBoosted() != null).collect(Collectors.toList());
-		Member booster = null;
-		long highestBoosterTime = 0;
-		for(var member : members) {
-			Timestamp serverBoostDate = Timestamp.valueOf(member.getTimeBoosted().toLocalDateTime());
-			if(serverBoostDate.getTime() > highestBoosterTime) {
-				highestBoosterTime = serverBoostDate.getTime();
-				booster = member;
+		var boostRole = DiscordRoles.SQLgetRole(e.getGuild().getIdLong(), "boo");
+		if(boostRole != 0) {
+			List<Member> passedBoosters = e.getGuild().getMembersWithRoles(e.getGuild().getRoleById(boostRole));
+			List<Member> boosters = e.getGuild().getBoosters();
+			var traitor = false;
+			for(final var member : passedBoosters) {
+				var currentBooster = boosters.parallelStream().filter(f -> f.getIdLong() == member.getIdLong()).findAny().orElse(null);
+				if(currentBooster == null) {
+					e.getGuild().removeRoleFromMember(member, e.getGuild().getRoleById(boostRole)).queue();
+					printMessage(e, member, false);
+					traitor = true;
+					break;
+				}
+			}
+			if(!traitor) {
+				for(final var member : boosters) {
+					var currentBooster = passedBoosters.parallelStream().filter(f -> f.getIdLong() == member.getIdLong()).findAny().orElse(null);
+					if(currentBooster == null) {
+						e.getGuild().addRoleToMember(member, e.getGuild().getRoleById(boostRole)).queue();
+						printMessage(e, member, true);
+						break;
+					}
+				}
+				
 			}
 		}
-		
+		else {
+			if(e.getNewBoostCount() > e.getOldBoostCount()) {
+				printMessage(e, null, true);
+			}
+			else {
+				printMessage(e, null, false);
+			}
+		}
+	}
+	
+	private void printMessage(GuildUpdateBoostCountEvent e, Member affectedMember, boolean increased) {
 		var channels = Azrael.SQLgetChannels(e.getGuild().getIdLong());
 		if(channels != null && channels.size() > 0) {
 			var log_channel = channels.parallelStream().filter(f -> f.getChannel_Type() != null && f.getChannel_Type().equals("log")).findAny().orElse(null);
 			if(log_channel != null) {
-				EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(293, 87, 63)).setTitle("Server has been boosted!");
-				message.addField("Booster", booster.getAsMention(), true);
+				EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(293, 87, 63)).setTitle((increased ? "Server has been boosted!" : "Tier level decreased!"));
+				if(affectedMember != null)message.addField((increased ? "Booster" : "Traitor"), affectedMember.getAsMention(), true);
 				message.addField("Current Boost Count", "**"+e.getNewBoostCount()+"**", true);
-				message.setThumbnail(booster.getUser().getEffectiveAvatarUrl());
+				message.setThumbnail(affectedMember.getUser().getEffectiveAvatarUrl());
+				message.setDescription((increased ? "A user has boosted our server! Party time!" : "There's a traitor among us who removed the boost!"));
 				e.getGuild().getTextChannelById(log_channel.getChannel_ID()).sendMessage(message.build()).queue();
 			}
 		}
-		
-		var boostRole = DiscordRoles.SQLgetRole(e.getGuild().getIdLong(), "boo");
-		if(boostRole != 0) e.getGuild().addRoleToMember(booster, e.getGuild().getRoleById(boostRole)).queue();
 	}
 }
