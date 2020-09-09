1a1cd16797d96af32677fce09f830fee996112df
Borrelli, Giuseppe-Emanuele <Giuseppe-Emanuele.Borrelli@toennies.de>
2020-09-09 09:13:56 +0200

    The daily command is also usable now when the ranking system is disabled

--- src/commands/Daily.java
+++ src/commands/Daily.java
@@ -72,7 +72,7 @@ public class Daily implements CommandPublic {
 				else
 					time_for_daily = -1;
 				//enter this block when the user is allowed to retrieve a daily again
-				if(time_for_daily < 0){
+				if(time_for_daily < 0) {
 					//confirm that there are any available rewards to send in private message (e.g. giveaways)
 					String cod_reward = RankingSystem.SQLRetrieveGiveawayReward(e.getGuild().getIdLong());
 					boolean exclude_cod = false;
@@ -86,111 +86,121 @@ public class Daily implements CommandPublic {
 						var tot_weight = 0;
 						//exclude rewards which get sent in private message, if there is no available reward
 						if(exclude_cod == true) {
-							//collect the total probability of all items together without special rewards (e.g. currency reward = 5% + special item = 1% = tot_weight = 6%)
-							tot_weight = daily_items.parallelStream().filter(i -> !i.getType().equals("cod")).mapToInt(i -> i.getWeight()).sum();
-							//remove all special items from the daily_items
-							daily_items = daily_items.parallelStream().filter(i -> !i.getType().equals("cod")).collect(Collectors.toList());
+							if(guild_settings.getRankingState()) {
+								//collect the total probability of all items together without special rewards (e.g. currency reward = 5% + special item = 1% = tot_weight = 6%)
+								tot_weight = daily_items.parallelStream().filter(i -> !i.getType().equals("cod")).mapToInt(i -> i.getWeight()).sum();
+								//remove all special items from the daily_items
+								daily_items = daily_items.parallelStream().filter(i -> !i.getType().equals("cod")).collect(Collectors.toList());
+							}
 						}
 						//get the weight of everything if special rewards are included
-						else
+						else if(guild_settings.getRankingState())
 							tot_weight = daily_items.parallelStream().mapToInt(i -> i.getWeight()).sum();
-						//get a random number between 1 and the total weight (probability)
-						int random = ThreadLocalRandom.current().nextInt(1, tot_weight);
-						
-						//iterate through the daily items and insert all items into the daily array depending by how big the probability is
-						//(e.g. if a currency reward equals 5% then it will be inserted 5 times into the array)
-						ArrayList<Dailies> list = new ArrayList<Dailies>();
-						for(Dailies daily : daily_items){
-							for(int i = 1; i <= daily.getWeight(); i++){
-								Dailies this_daily = new Dailies();
-								this_daily.setDescription(daily.getDescription());
-								this_daily.SetType(daily.getType());
-								this_daily.setAction(daily.getAction());
-								list.add(this_daily);
-							}
-						}
-						//get the index of an array depending on the random number and draw the reward into the screen
-						DrawDaily.draw(e, list.get(random).getDescription(), guild_settings);
-						//set the daily reset to next midnight time
-						long time = System.currentTimeMillis();
-						Timestamp timestamp = new Timestamp(time);
-						LocalTime midnight = LocalTime.MIDNIGHT;
-						LocalDate today = LocalDate.now();
-						LocalDateTime tomorrowMidnight = LocalDateTime.of(today, midnight).plusDays(1);
-						Timestamp timestamp2 = Timestamp.valueOf(tomorrowMidnight);
-						var editedRows = 0;
-						//if it's a currency reward, add it directly to the total currency of the user and update the db
-						if(list.get(random).getType().equals("cur")) {
-							constructors.Rank user_details = RankingSystem.SQLgetWholeRankView(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong());
-							user_details.setCurrency(user_details.getCurrency()+Long.parseLong(list.get(random).getDescription().replaceAll("[^0-9]*", "")));
-							user_details.setLastUpdate(timestamp);
-							editedRows = RankingSystem.SQLUpdateCurrency(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), user_details.getCurrency(), user_details.getLastUpdate());
-							if(editedRows > 0)
-								Hashes.addRanking(e.getGuild().getId()+"_"+e.getMember().getUser().getIdLong(), user_details);
-						}
-						//if it's a experience boost reward, add it to inventory and activate it, if the action is set to 'use'
-						else if(list.get(random).getType().equals("exp")) {
-							var item_id = RankingSystem.SQLgetSkinshopContentAndType(e.getGuild().getIdLong(), guild_settings.getThemeID()).parallelStream().filter(i -> i.getShopDescription().equals(list.get(random).getDescription())).findAny().orElse(null).getItemID();
-							if(list.get(random).getAction().equals("keep")) {
-								InventoryContent inventory = RankingSystem.SQLgetNumberAndExpirationFromInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), list.get(random).getDescription(), "perm", guild_settings.getThemeID());
-								if(inventory != null)
-									editedRows = RankingSystem.SQLInsertInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, inventory.getNumber()+1, "perm", guild_settings.getThemeID());
-								else
-									editedRows = RankingSystem.SQLInsertInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, 1, "perm", guild_settings.getThemeID());
-							}
-							else if(list.get(random).getAction().equals("use")) {
-								InventoryContent inventory = RankingSystem.SQLgetNumberAndExpirationFromInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), list.get(random).getDescription(), "limit", guild_settings.getThemeID());
-								try {
-									Timestamp timestamp3 = new Timestamp(inventory.getExpiration().getTime()+1000*60*60*24);
-									editedRows = RankingSystem.SQLInsertInventoryWithLimit(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, inventory.getNumber()+1, "limit", timestamp3, guild_settings.getThemeID());
-								} catch(NullPointerException npe){
-									Timestamp timestamp4 = new Timestamp(time+1000*60*60*24);
-									editedRows = RankingSystem.SQLInsertInventoryWithLimit(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, 1, "limit", timestamp4, guild_settings.getThemeID());
+						//eclude exp and cur items, if the ranking system is disabled
+						else
+							tot_weight = daily_items.parallelStream().filter(i -> !i.getType().equals("cur") && !i.getType().equals("exp")).mapToInt(i -> i.getWeight()).sum();
+						if(tot_weight > 0) {
+							//get a random number between 1 and the total weight (probability)
+							int random = ThreadLocalRandom.current().nextInt(1, tot_weight);
+							
+							//iterate through the daily items and insert all items into the daily array depending by how big the probability is
+							//(e.g. if a currency reward equals 5% then it will be inserted 5 times into the array)
+							ArrayList<Dailies> list = new ArrayList<Dailies>();
+							for(Dailies daily : daily_items){
+								for(int i = 1; i <= daily.getWeight(); i++){
+									Dailies this_daily = new Dailies();
+									this_daily.setDescription(daily.getDescription());
+									this_daily.SetType(daily.getType());
+									this_daily.setAction(daily.getAction());
+									list.add(this_daily);
 								}
 							}
-						}
-						//if it's a special reward, send it in private message to the user and print it in the log channel at the same time
-						else if(list.get(random).getType().equals("cod")) {
-							//log the reward in bot channel and send a private message
-							e.getMember().getUser().openPrivateChannel().queue(channel -> {
-								channel.sendMessage(STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD)+cod_reward).queue(success -> {
-									EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(268, 81, 88)).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_DAILY));
-									STATIC.writeToRemoteChannel(e.getGuild(), message, STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD_SENT).replaceFirst("\\{\\}", e.getMember().getUser().getName()+"#"+e.getMember().getUser().getDiscriminator()).replace("{}", e.getMember().getUser().getId())+cod_reward, Channel.LOG.getType());
-									channel.close().queue();
-								}, error -> {
-									STATIC.writeToRemoteChannel(e.getGuild(), new EmbedBuilder().setColor(Color.RED), STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD_NOT_SENT).replaceFirst("\\{\\}", e.getMember().getUser().getName()+"#"+e.getMember().getUser().getDiscriminator()).replace("{}", e.getMember().getUser().getId())+cod_reward, Channel.LOG.getType());
-									channel.close().queue();
+							//get the index of an array depending on the random number and draw the reward into the screen
+							DrawDaily.draw(e, list.get(random).getDescription(), guild_settings);
+							//set the daily reset to next midnight time
+							long time = System.currentTimeMillis();
+							Timestamp timestamp = new Timestamp(time);
+							LocalTime midnight = LocalTime.MIDNIGHT;
+							LocalDate today = LocalDate.now();
+							LocalDateTime tomorrowMidnight = LocalDateTime.of(today, midnight).plusDays(1);
+							Timestamp timestamp2 = Timestamp.valueOf(tomorrowMidnight);
+							var editedRows = 0;
+							//if it's a currency reward, add it directly to the total currency of the user and update the db
+							if(list.get(random).getType().equals("cur")) {
+								constructors.Rank user_details = RankingSystem.SQLgetWholeRankView(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong());
+								user_details.setCurrency(user_details.getCurrency()+Long.parseLong(list.get(random).getDescription().replaceAll("[^0-9]*", "")));
+								user_details.setLastUpdate(timestamp);
+								editedRows = RankingSystem.SQLUpdateCurrency(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), user_details.getCurrency(), user_details.getLastUpdate());
+								if(editedRows > 0)
+									Hashes.addRanking(e.getGuild().getId()+"_"+e.getMember().getUser().getIdLong(), user_details);
+							}
+							//if it's a experience boost reward, add it to inventory and activate it, if the action is set to 'use'
+							else if(list.get(random).getType().equals("exp")) {
+								var item_id = RankingSystem.SQLgetSkinshopContentAndType(e.getGuild().getIdLong(), guild_settings.getThemeID()).parallelStream().filter(i -> i.getShopDescription().equals(list.get(random).getDescription())).findAny().orElse(null).getItemID();
+								if(list.get(random).getAction().equals("keep")) {
+									InventoryContent inventory = RankingSystem.SQLgetNumberAndExpirationFromInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), list.get(random).getDescription(), "perm", guild_settings.getThemeID());
+									if(inventory != null)
+										editedRows = RankingSystem.SQLInsertInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, inventory.getNumber()+1, "perm", guild_settings.getThemeID());
+									else
+										editedRows = RankingSystem.SQLInsertInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, 1, "perm", guild_settings.getThemeID());
+								}
+								else if(list.get(random).getAction().equals("use")) {
+									InventoryContent inventory = RankingSystem.SQLgetNumberAndExpirationFromInventory(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), list.get(random).getDescription(), "limit", guild_settings.getThemeID());
+									try {
+										Timestamp timestamp3 = new Timestamp(inventory.getExpiration().getTime()+1000*60*60*24);
+										editedRows = RankingSystem.SQLInsertInventoryWithLimit(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, inventory.getNumber()+1, "limit", timestamp3, guild_settings.getThemeID());
+									} catch(NullPointerException npe){
+										Timestamp timestamp4 = new Timestamp(time+1000*60*60*24);
+										editedRows = RankingSystem.SQLInsertInventoryWithLimit(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), item_id, timestamp, 1, "limit", timestamp4, guild_settings.getThemeID());
+									}
+								}
+							}
+							//if it's a special reward, send it in private message to the user and print it in the log channel at the same time
+							else if(list.get(random).getType().equals("cod")) {
+								//log the reward in bot channel and send a private message
+								e.getMember().getUser().openPrivateChannel().queue(channel -> {
+									channel.sendMessage(STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD)+cod_reward).queue(success -> {
+										EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(268, 81, 88)).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_DAILY));
+										STATIC.writeToRemoteChannel(e.getGuild(), message, STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD_SENT).replaceFirst("\\{\\}", e.getMember().getUser().getName()+"#"+e.getMember().getUser().getDiscriminator()).replace("{}", e.getMember().getUser().getId())+cod_reward, Channel.LOG.getType());
+										channel.close().queue();
+									}, error -> {
+										STATIC.writeToRemoteChannel(e.getGuild(), new EmbedBuilder().setColor(Color.RED), STATIC.getTranslation(e.getMember(), Translation.DAILY_REWARD_NOT_SENT).replaceFirst("\\{\\}", e.getMember().getUser().getName()+"#"+e.getMember().getUser().getDiscriminator()).replace("{}", e.getMember().getUser().getId())+cod_reward, Channel.LOG.getType());
+										channel.close().queue();
+									});
 								});
-							});
-							
-							//mark the code as used
-							editedRows = RankingSystem.SQLUpdateUsedOnReward(cod_reward, e.getGuild().getIdLong());
-						}
-						//If the player was unlucky, send a message that nothing was won
-						else if(list.get(random).getType().equals("riv")) {
-							EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(268, 81, 88)).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_DAILY));
-							e.getChannel().sendMessage(message.setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_RIVET)).build()).queue();
-							//mark as successful
-							editedRows = 1;
-						}
-						//if the inventory has been updated, set the daily for this user and the current day as opened
-						if(editedRows > 0) {
-							if(RankingSystem.SQLInsertDailiesUsage(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), timestamp, timestamp2) > 0) {
-								logger.debug("{} received {} out of the Daily command", e.getMember().getUser().getId(), list.get(random).getDescription());
-								RankingSystem.SQLInsertActionLog("low", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily retrieved", list.get(random).getDescription());
+								
+								//mark the code as used
+								editedRows = RankingSystem.SQLUpdateUsedOnReward(cod_reward, e.getGuild().getIdLong());
+							}
+							//If the player was unlucky, send a message that nothing was won
+							else if(list.get(random).getType().equals("riv")) {
+								EmbedBuilder message = new EmbedBuilder().setColor(Color.getHSBColor(268, 81, 88)).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_DAILY));
+								e.getChannel().sendMessage(message.setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_RIVET)).build()).queue();
+								//mark as successful
+								editedRows = 1;
+							}
+							//if the inventory has been updated, set the daily for this user and the current day as opened
+							if(editedRows > 0) {
+								if(RankingSystem.SQLInsertDailiesUsage(e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), timestamp, timestamp2) > 0) {
+									logger.debug("{} received {} out of the Daily command", e.getMember().getUser().getId(), list.get(random).getDescription());
+									RankingSystem.SQLInsertActionLog("low", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily retrieved", list.get(random).getDescription());
+								}
+								else {
+									STATIC.writeToRemoteChannel(e.getGuild(), new EmbedBuilder().setColor(Color.RED).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_ERROR)), STATIC.getTranslation(e.getMember(), Translation.DAILY_ERROR_2)+e.getMember().getAsMention(), Channel.LOG.getType());
+									logger.error("used dailies from {} couldn't be marked in RankingSystem.dailies_usage table", e.getMember().getUser().getId());
+									RankingSystem.SQLInsertActionLog("high", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily retrieval not marked", list.get(random).getDescription());
+								}
 							}
 							else {
-								STATIC.writeToRemoteChannel(e.getGuild(), new EmbedBuilder().setColor(Color.RED).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_ERROR)), STATIC.getTranslation(e.getMember(), Translation.DAILY_ERROR_2)+e.getMember().getAsMention(), Channel.LOG.getType());
-								logger.error("used dailies from {} couldn't be marked in RankingSystem.dailies_usage table", e.getMember().getUser().getId());
-								RankingSystem.SQLInsertActionLog("high", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily retrieval not marked", list.get(random).getDescription());
+								e.getChannel().sendMessage(new EmbedBuilder().setColor(Color.RED).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_ERROR)).setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_ERROR_3)).build()).queue();
+								logger.error("{} couldn't be inserted into inventory", list.get(random).getDescription());
+								RankingSystem.SQLInsertActionLog("high", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily item couldn't be inserted to inventory", "An insert error occurred for the following item "+list.get(random).getDescription());
 							}
+							list.clear();
 						}
 						else {
-							e.getChannel().sendMessage(new EmbedBuilder().setColor(Color.RED).setTitle(STATIC.getTranslation(e.getMember(), Translation.EMBED_TITLE_ERROR)).setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_ERROR_3)).build()).queue();
-							logger.error("{} couldn't be inserted into inventory", list.get(random).getDescription());
-							RankingSystem.SQLInsertActionLog("high", e.getMember().getUser().getIdLong(), e.getGuild().getIdLong(), "Daily item couldn't be inserted to inventory", "An insert error occurred for the following item "+list.get(random).getDescription());
+							e.getChannel().sendMessage(new EmbedBuilder().setColor(Color.RED).setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_EMPTY)).build()).queue();
 						}
-						list.clear();
 					}
 					else {
 						e.getChannel().sendMessage(new EmbedBuilder().setColor(Color.RED).setDescription(STATIC.getTranslation(e.getMember(), Translation.DAILY_EMPTY)).build()).queue();
